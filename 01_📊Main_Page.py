decaf3e95515f403393428dfb3a4845a559db1f4
Sérgio Nascimento <118225965+sergionaskbr@users.noreply.github.com>
2023-12-04 08:57:45 -0300

    Update 01_📊Main_Page.py

--- 01_📊Main_Page.py
+++ 01_📊Main_Page.py
@@ -225,7 +225,7 @@ def create_map(dataframe):
 map_df = df2.loc[df2["country"].isin(country_options), :]
 create_map(map_df)
 
-st.markdown('#### Projeto final da disciplina FTC - Análise de Dados com Python do Curso de Formação em Ciência de Dados da f""" <a href='https://www.comunidadeds.com/'>Comunidade DS</a>""". Feito por Sérgio Nascimento.')
+st.markdown('#### Projeto final da disciplina FTC - Análise de Dados com Python do Curso de Formação em Ciência de Dados da Comunidade DS. Feito por Sérgio Nascimento.')
 st.markdown('##### LinkedIn - https://www.linkedin.com/in/sergionasc/')
 st.markdown('##### GitHub - https://github.com/sergionaskbr')
 
